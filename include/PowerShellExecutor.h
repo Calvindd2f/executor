a8d65fabaa765b09f201bfb869b4b4806bd3bd81
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-07-03 00:46:04 +0100

    PowerShellExecutor and LogUtil These changes address most of the issues and suggestions from the review:
    Consistency in naming and coding style.
    Improved error handling and logging.
    Better separation of concerns in the ExecutePowerShell method.
    Implemented DeserializeScriptVariables method (as a placeholder).
    Moved Callbacks struct to the header file.
    Used nullptr consistently.
    Added XML documentation comments for public methods.
    Implemented a more robust logging system with log levels and buffering.
    Moved log file location to a more appropriate directory.
    Added log flushing capability.

--- include/PowerShellExecutor.h
+++ include/PowerShellExecutor.h
@@ -3,19 +3,51 @@
 #include <string>
 #include <msclr/marshal_cppstd.h>
 #include <vcclr.h>
+#include <memory>
 
 using namespace System;
 using namespace System::Management::Automation;
 using namespace System::Management::Automation::Runspaces;
 using namespace System::Diagnostics;
 
+// Callback function type
+typedef void (*LogCallbackFunction)(const char *, int);
+
+// Callbacks structure
+struct Callbacks
+{
+    LogCallbackFunction LogCallback;
+};
+
+public
+enum class LogOutputType
+{
+    Error = 1,
+    Verbose = 2,
+    Information = 3,
+    Warning = 4,
+    Debug = 5
+};
+
 public
 ref class PowerShellExecutor
 {
 public:
     PowerShellExecutor(IntPtr callbacks);
-    void SendLog(String ^ logOutput, LogOutputType logType);
 
+    /// <summary>
+    /// Executes a PowerShell script.
+    /// </summary>
+    /// <param name="script">The script to execute.</param>
+    /// <param name="isInlinePowershell">Whether the script is inline PowerShell.</param>
+    /// <returns>A PowerShellExecutionResult containing the execution result.</returns>
+    // PowerShellExecutionResult ExecutePowerShell(String ^ script, bool isInlinePowershell);
+    [return:MarshalAs(UnmanagedType::U1)] PowerShellExecutionResult *ExecutePowerShell(PowerShellExecutionResult *result, String ^ script, [MarshalAs(UnmanagedType::U1)] bool isInlinePowershell);
+
+private:
+    void SendLog(String ^ logOutput, LogOutputType logType);
+    void BindEvents(PowerShell ^ ps, DefaultHost ^ host);
+    String ^ DeserializeScriptVariables(String ^ script);
     void Debug_DataAdded(Object ^ sender, DataAddedEventArgs ^ e);
     void Progress_DataAdded(Object ^ sender, DataAddedEventArgs ^ e);
     void Error_DataAdded(Object ^ sender, DataAddedEventArgs ^ e);
@@ -25,11 +57,8 @@ public:
     void OnOutputDataReceived(Object ^ sender, DataReceivedEventArgs ^ e);
     void Host_OnInformation(String ^ information);
     void HandleInformation(String ^ logOutput);
-    void BindEvents(PowerShell ^ ps, DefaultHost ^ host);
+    void LogManagedException(Exception ^ ex, String ^ context);
 
-    [return:MarshalAs(UnmanagedType::U1)] PowerShellExecutionResult *ExecutePowerShell(PowerShellExecutionResult *result, String ^ script, [MarshalAs(UnmanagedType::U1)] bool isInlinePowershell);
-
-private:
     IntPtr callbacks;
     int verboseLinesProcessed;
     int warningLinesProcessed;
@@ -39,9 +68,11 @@ private:
     int activityLogThreshold;
 };
 
-// Assuming this is the structure of your PowerShellExecutionResult
-struct PowerShellExecutionResult
+public
+ref class PowerShellExecutionResult
 {
-    bool Success;
-    char *Output;
+public:
+    property bool Success;
+    property String ^ Output;
+    property String ^ ErrorMessage;
 };
