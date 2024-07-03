9d84a3ffec0e886c0988f8d6aa990325913a1740
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-07-03 01:06:51 +0100

    + Consider adding a Debug type to align with common logging practices. + Add XML documentation comments to describe each enum value.

--- managed/LogOutputType.cs
+++ managed/LogOutputType.cs
@@ -1,10 +1,29 @@
 namespace PowerShellExecutorNamespace
 {
+    /// <summary>
+    /// Represents an enumeration for different types of log output.
+    /// </summary>
     public enum LogOutputType
     {
+        /// <summary>
+        /// Error log output.
+        /// </summary>
         Error = 1,
+        /// <summary>
+        /// Verbose log output.
+        /// </summary>
         Verbose = 2,
+        /// <summary>
+        /// Information log output.
+        /// </summary>
         Information = 3,
-        Warning = 4
+        /// <summary>
+        /// Warning log output.
+        /// </summary>
+        Warning = 4,
+        /// <summary>
+        /// Debug log output.
+        /// </summary>
+        Debug = 5
     }
 }
