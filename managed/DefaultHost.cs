f9121bb5254a634c0fd70f6f2fcc0d7f7302e9e0
​ <74103103+Calvindd2f@users.noreply.github.com>
2024-07-03 01:02:16 +0100

    Good use of XML documentation comments. Proper implementation of the PSHost abstract class. Thread-safe event handling for OnInformation.

--- managed/DefaultHost.cs
+++ managed/DefaultHost.cs
@@ -8,101 +8,101 @@ using System.Management.Automation.Host;
 namespace DefaultHost : PSHost
 {
     private CultureInfo _currentCulture;
-    private CultureInfo _currentUICulture;
-    private InformationDelegate _onInformation;
+private CultureInfo _currentUICulture;
+private InformationDelegate _onInformation;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="DefaultHost"/> class.
-    /// </summary>
-    /// <param name="currentCulture">The current culture.</param>
-    /// <param name="currentUICulture">The current UI culture.</param>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="currentCulture"/> is null.</exception>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="currentUICulture"/> is null.</exception>
-    public DefaultHost(CultureInfo currentCulture, CultureInfo currentUICulture)
+/// <summary>
+/// Initializes a new instance of the <see cref="DefaultHost"/> class.
+/// </summary>
+/// <param name="currentCulture">The current culture.</param>
+/// <param name="currentUICulture">The current UI culture.</param>
+/// <exception cref="ArgumentNullException">Thrown if <paramref name="currentCulture"/> is null.</exception>
+/// <exception cref="ArgumentNullException">Thrown if <paramref name="currentUICulture"/> is null.</exception>
+public DefaultHost(CultureInfo currentCulture, CultureInfo currentUICulture)
+{
+    _currentCulture = currentCulture ?? throw new ArgumentNullException(nameof(currentCulture));
+    _currentUICulture = currentUICulture ?? throw new ArgumentNullException(nameof(currentUICulture));
+}
+
+public event InformationDelegate OnInformation
+{
+    [MethodImpl(MethodImplOptions.Synchronized)]
+    add
     {
-        _currentCulture = currentCulture ?? throw new ArgumentNullException(nameof(currentCulture));
-        _currentUICulture = currentUICulture ?? throw new ArgumentNullException(nameof(currentUICulture));
+        _onInformation = (InformationDelegate)Delegate.Combine(_onInformation, value);
+        GC.KeepAlive(this);
     }
-
-    public event InformationDelegate OnInformation
+    [MethodImpl(MethodImplOptions.Synchronized)]
+    remove
     {
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        add
-        {
-            _onInformation = (InformationDelegate)Delegate.Combine(_onInformation, value);
-            GC.KeepAlive(this);
-        }
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        remove
-        {
-            _onInformation = (InformationDelegate)Delegate.Remove(_onInformation, value);
-            GC.KeepAlive(this);
-        }
+        _onInformation = (InformationDelegate)Delegate.Remove(_onInformation, value);
+        GC.KeepAlive(this);
     }
+}
 
-    protected void RaiseOnInformation(string value)
+protected void RaiseOnInformation(string value)
+{
+    _onInformation?.Invoke(value);
+}
+
+private void HandleInformation(string information)
+{
+    RaiseOnInformation(information);
+}
+
+public override string Name => "Default Host";
+
+public override Version Version => new Version(1, 0);
+
+public override Guid InstanceId => Guid.NewGuid();
+
+public override PSHostUserInterface UI
+{
+    get
     {
-        _onInformation?.Invoke(value);
+        var defaultHostUserInterface = new DefaultHostUserInterface();
+        defaultHostUserInterface.OnInformation += HandleInformation;
+        GC.KeepAlive(this);
+        return defaultHostUserInterface;
     }
+}
 
-    private void HandleInformation(string information)
-    {
-        RaiseOnInformation(information);
-    }
+public override CultureInfo CurrentCulture
+{
+    get => _currentCulture;
+    set => _currentCulture = value ?? throw new ArgumentNullException(nameof(value));
+}
 
-    public override string Name => "Default Host";
+public override CultureInfo CurrentUICulture
+{
+    get => _currentUICulture;
+    set => _currentUICulture = value ?? throw new ArgumentNullException(nameof(value));
+}
 
-    public override Version Version => new Version(1, 0);
+public override void SetShouldExit(int exitCode)
+{
+    throw new NotSupportedException("Not implemented.");
+}
 
-    public override Guid InstanceId => Guid.NewGuid();
+public override void EnterNestedPrompt()
+{
+    throw new NotSupportedException("Nested prompt attempted.");
+}
 
-    public override PSHostUserInterface UI
-    {
-        get
-        {
-            var defaultHostUserInterface = new DefaultHostUserInterface();
-            defaultHostUserInterface.OnInformation += HandleInformation;
-            GC.KeepAlive(this);
-            return defaultHostUserInterface;
-        }
-    }
+public override void ExitNestedPrompt()
+{
+    throw new NotSupportedException("Not implemented.");
+}
 
-    public override CultureInfo CurrentCulture
-    {
-        get => _currentCulture;
-        set => _currentCulture = value ?? throw new ArgumentNullException(nameof(value));
-    }
+public override void NotifyBeginApplication()
+{
+    throw new NotSupportedException("Not implemented.");
+}
 
-    public override CultureInfo CurrentUICulture
-    {
-        get => _currentUICulture;
-        set => _currentUICulture = value ?? throw new ArgumentNullException(nameof(value));
-    }
+public override void NotifyEndApplication()
+{
+    throw new NotSupportedException("Exit prompt attempted.");
+}
 
-    public override void SetShouldExit(int exitCode)
-    {
-       throw new NotSupportedException("Not implemented.");
-    }
-
-    public override void EnterNestedPrompt()
-    {
-        throw new NotSupportedException("Nested prompt attempted.");
-    }
-
-    public override void ExitNestedPrompt()
-    {
-        throw new NotSupportedException("Not implemented.");
-    }
-
-    public override void NotifyBeginApplication()
-    {
-        throw new NotSupportedException("Not implemented.");
-    }
-
-    public override void NotifyEndApplication()
-    {
-        throw new NotSupportedException("Exit prompt attempted.");
-    }
-
-    public delegate void InformationDelegate(string information);
+public delegate void InformationDelegate(string information);
 }
